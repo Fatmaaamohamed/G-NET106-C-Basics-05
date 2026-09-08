ec1c853bf0082ae8bdac806e5091be77a14abbeb
Fatma_Mohamed <fm5252498@gmail.com>
2026-09-08 16:03:51 +0300

    Question01

---
+++ G-NET106-C#Basics-05/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G_NET106_C_Basics_05
+{
+    internal class Book
+    {
+        private string password = "secret";
+    }
+}
--- G-NET106-C#Basics-05/Program.cs
+++ G-NET106-C#Basics-05/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Question01
+            /*Add a private string password = "secret"; field to a Book class. Try to print it from Main
+            (outside the class). What happens, and why?*/
+
+            Book book = new Book();
+            book.password = "secret";
+            //it gives Compile error bc Password is inaccessible due to protection level because the acces modifier private makes the field only accessible at its scope (class)
+            #endregion
         }
     }
 }
